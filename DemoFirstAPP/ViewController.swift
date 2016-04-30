b5af3f0671f115a8da268066354fe8211f6b102e
PaulHo <paulho1223@gmail.com>
2016-04-30 11:07:50 +0800

    Initial Commit

---
+++ DemoFirstAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoFirstAPP
+//
+//  Created by PaulHo on 2016/4/30.
+//  Copyright © 2016年 PaulHo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
